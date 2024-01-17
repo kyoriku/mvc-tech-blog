521324ec7b7d40b54bfc47271c17dca48d5c67ef
Austin Graham <austiingraham@gmail.com>
2024-01-17 08:04:03 -0500

    updated homeRoutes.js with post details route, added authentication middleware, temporarily bypassing withAuth for testing purposes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { User, Post, Comment } = require('../models');
+const withAuth = require("../utils/auth");
 
 router.get('/', async (req, res) => {
   try {
@@ -13,9 +14,39 @@ router.get('/', async (req, res) => {
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
+    // res.status(200).json(posts); // For Insomnia testing purposes
 
     res.render('homepage', { 
       posts, 
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// router.get("/post/:id", withAuth, async (req, res) => { // Temporarily bypassing withAuth for testing
+  router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"]
+        },
+        {
+          model: Comment,
+          include: [
+            { model: User, 
+              attributes: ["username"] }],
+        },
+      ],
+    });
+    const post = postData.get({ plain: true });
+    // res.status(200).json(post); // For Insomnia testing purposes
+
+    res.render("post", {
+      ...post,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
