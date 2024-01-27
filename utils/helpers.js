265a80963eb9c6aad51dbfdf22fb25ddad0188bd
Austin Graham <austiingraham@gmail.com>
2024-01-27 02:13:36 -0500

    added initial layout for post.handlebars, updated format_date function, increased session maxAge to prevent being logged out while developing

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,25 @@
 module.exports = {
-  format_date: (date) => {
-    const options = { 
-      weekday: 'long', 
-      month: 'long', 
-      day: 'numeric', 
-      year: 'numeric' 
+  formatDate: (date) => {
+    const options = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric'
     };
     return date.toLocaleDateString('en-US', options);
   },
-  format_time: (date) => {
+  formatTime: (date) => {
     return date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' });
+  },
+  formatDateTime: (date) => {
+    const options = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    };
+    return date.toLocaleDateString('en-US', options);
   },
 };
